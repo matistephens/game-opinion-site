7bffe181e7860015c16222076f566008dc77c562
Matias Stephens <matias.stur@gmail.com>
2022-06-01 12:50:48 -0400

    Fix Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,31 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Games Opinion</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/opiniones">Opiniones</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/administracion">Administración</router-link>
-          </li>
-        </ul>
-      </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Games Opinion</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="true"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/"
+            >Home<span class="sr-only">(current)</span></router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/opiniones">Opiniones</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/administracion">Administración</router-link>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
